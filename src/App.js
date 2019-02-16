183768c784b94a874cbd3836358e15384b51c616
AntonioGarnier <alu0100454437@ull.edu.es>
2019-02-16 22:38:15 +0000

    master #comment Refactor: now App has the routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import ToolbarComponent from './components/ToolbarComponent'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import Main from './screens/Main'
+import Home from './screens/Home'
+import Work from './screens/Work'
+import Contact from './screens/Contact'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ToolbarComponent/>
-      </div>
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route sensitive path="/work" component={Work} />
+            <Route sensitive path="/contact" component={Contact} />
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        </Main>
     );
   }
 }
 
-export default App;
+export default withRouter(App)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ToolbarComponent from './components/ToolbarComponent'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
